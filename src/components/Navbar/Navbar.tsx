2b1c9bed6f6f7a04f054bf7afeba2cb5f71df8de
mohcine elbah <mohcine.elbah@gmail.com>
2024-06-07 08:41:15 +0200

    Navbar customisation

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import {
 } from "@/components/ui/tooltip"
 import { Home, LineChart, Package, Package2, Settings, ShoppingCart, Users2 } from "lucide-react";
 import { ModeToggle } from "../ui/DarkMode/ModeToggle";
+import { Link } from "react-router-dom";
 
 export function Navbar() {
 
@@ -17,68 +18,44 @@ export function Navbar() {
           className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
         >
           <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
-          <span className="sr-only">Acme Inc</span>
+          <span className="sr-only">Gesto</span>
         </a>
         <TooltipProvider>
           <Tooltip>
             <TooltipTrigger asChild>
-              <a
-                href="#"
+              <Link
+                to="/"
                 className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
               >
                 <Home className="h-5 w-5" />
                 <span className="sr-only">Dashboard</span>
-              </a>
+              </Link>
             </TooltipTrigger>
             <TooltipContent side="right">Dashboard</TooltipContent>
           </Tooltip>
           <Tooltip>
             <TooltipTrigger asChild>
-              <a
-                href="#"
+              <Link
+                to="/products"
                 className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
               >
-                <ShoppingCart className="h-5 w-5" />
-                <span className="sr-only">Orders</span>
-              </a>
-            </TooltipTrigger>
-            <TooltipContent side="right">Orders</TooltipContent>
-          </Tooltip>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <a
-                href="#"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-              >
                 <Package className="h-5 w-5" />
-                <span className="sr-only">Products</span>
-              </a>
+                <span className="sr-only">Produits</span>
+              </Link>
             </TooltipTrigger>
-            <TooltipContent side="right">Products</TooltipContent>
+            <TooltipContent side="right">Produits</TooltipContent>
           </Tooltip>
           <Tooltip>
             <TooltipTrigger asChild>
-              <a
-                href="#"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-              >
-                <Users2 className="h-5 w-5" />
-                <span className="sr-only">Customers</span>
-              </a>
-            </TooltipTrigger>
-            <TooltipContent side="right">Customers</TooltipContent>
-          </Tooltip>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <a
-                href="#"
+              <Link
+                to="#"
                 className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
               >
                 <LineChart className="h-5 w-5" />
-                <span className="sr-only">Analytics</span>
-              </a>
+                <span className="sr-only">Statistiques</span>
+              </Link>
             </TooltipTrigger>
-            <TooltipContent side="right">Analytics</TooltipContent>
+            <TooltipContent side="right">Statistiques</TooltipContent>
           </Tooltip>
         </TooltipProvider>
       </nav>
@@ -91,10 +68,10 @@ export function Navbar() {
                 className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
               >
                 <Settings className="h-5 w-5" />
-                <span className="sr-only">Settings</span>
+                <span className="sr-only">Paramétres</span>
               </a>
             </TooltipTrigger>
-            <TooltipContent side="right">Settings</TooltipContent>
+            <TooltipContent side="right">Paramétres</TooltipContent>
           </Tooltip>
         </TooltipProvider>
         <ModeToggle />
