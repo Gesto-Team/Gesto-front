0600942b137eb137c6700ff803658e9ddc704266
mohcine <mohcine.elbah@ynov.com>
2024-08-28 18:46:35 +0100

    Export and filter by buttons removed v2

--- src/components/ProductsList/Products.tsx
+++ src/components/ProductsList/Products.tsx
@@ -1,17 +1,4 @@
 import {
-  File,
-  ListFilter,
-} from "lucide-react"
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import {
   Tabs,
   TabsList,
   TabsTrigger,
